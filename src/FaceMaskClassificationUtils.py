63a40d4710f2c9e9d7ec001a69df852a57a82219
Amir Dahan <amird1234@gmail.com>
2022-02-17 02:16:20 +0200

    support Natural Images Data
    Signed-off-by: Amir Dahan <amird1234@gmail.com>

--- src/FaceMaskClassificationUtils.py
+++ src/FaceMaskClassificationUtils.py
@@ -1,8 +1,14 @@
-import torch
-from torchvision import transforms
+import copy
+import os
+
 import matplotlib.pyplot as plt
-import numpy as np
 from PIL import ImageFont, ImageDraw
+
+import torch
+import numpy as np
+from torchvision import datasets, transforms
+import torchvision
+import torch.nn as nn
 
 MODEL_PATH = '/content/drive/MyDrive/colab/GenderClassification/New/output/model.pth'
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -62,3 +68,219 @@ def draw_bounding_boxes(img, box, label):
     draw.text((box[0], box[1]-round(w/35)), label, (255, 0, 0), font=font)
 
     return img_draw
+
+
+def split(dataset, _train_size=0.7, _validation_size=0.15, _test_size=0.15):
+    train_ds = int(_train_size * len(dataset))
+    validation_ds = int(_validation_size * len(dataset))
+    test_ds = len(dataset) - train_ds - validation_ds
+
+    print("len: train valid test:" + str(train_ds) + " " + str(validation_ds) + " " + str(test_ds))
+    train_data, validation_data, test_data = torch.utils.data.random_split(dataset, [train_ds, validation_ds, test_ds])
+    print("dataset is now " + str(len(train_data)))
+    return train_data, validation_data, test_data
+
+
+def split_prepare_dataset(data_dir, num_workers, batch_size):
+    train_transform = transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=mean,
+                             std=std)
+    ])
+
+    test_transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=mean,
+                             std=std)
+    ])
+
+    all_data = datasets.ImageFolder(root=data_dir, transform=train_transform)
+    train_data, validation_data, test_data = split(all_data)
+
+    train_loader = torch.utils.data.DataLoader(train_data,
+                                               batch_size=batch_size,
+                                               shuffle=True,
+                                               num_workers=num_workers)
+
+    validation_loader = torch.utils.data.DataLoader(validation_data,
+                                                    batch_size=batch_size,
+                                                    shuffle=True,
+                                                    num_workers=num_workers)
+
+    test_loader = torch.utils.data.DataLoader(test_data,
+                                              batch_size=batch_size,
+                                              shuffle=True,
+                                              num_workers=num_workers)
+
+    dataloaders = {
+        'train': train_loader,
+        'validation': validation_loader,
+        'test': test_loader,
+    }
+
+    total_batch_sizes = {'train': len(train_loader), 'validation': len(validation_loader), 'test': len(test_loader)}
+
+    print(train_data)
+    print(test_data)
+
+    class_names = all_data.classes
+
+    print("Class Names: " + str(class_names))
+
+    return dataloaders, total_batch_sizes, class_names
+
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs, dataloaders, total_batch_sizes, batch_size):
+    model = model.to(DEVICE)
+    best_acc = 0.0
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_epoch = 0
+    measurements = {'Loss': {'train': [], 'validation': []}, 'Accuracy': {'train': [], 'validation': []}}
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        for phase in ['train', 'validation']:
+            if phase == 'train':
+                scheduler.step()
+                model.train()
+
+            else:
+                model.eval()
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(DEVICE)
+                labels = labels.to(DEVICE)
+
+                optimizer.zero_grad()
+
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / total_batch_sizes[phase]
+            epoch_acc = running_corrects.double() / (total_batch_sizes[phase] * batch_size)
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # save measurements for later
+            measurements['Accuracy'][phase].append(epoch_acc)
+            measurements['Loss'][phase].append(epoch_loss)
+
+
+            if phase == 'validation' and epoch_acc > best_acc:
+                best_epoch = epoch
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+    print('Training complete')
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    for m in measurements:
+        for phase in ['train', 'validation']:
+            plt.clf()
+            plt.figure(figsize=(6, 6))
+            plt.plot([x for x in range(len(measurements[m][phase]))], measurements[m][phase], '-')
+            if m == 'Accuracy':
+                plt.plot(best_epoch, measurements[m][phase][best_epoch].item(), 'g*')
+            else:
+                plt.plot(best_epoch, measurements[m][phase][best_epoch], 'g*')
+            plt.title(phase + " " + m)
+            #plt.show()
+            plt.savefig(os.path.join('/home/adahan/Project/report/', "FaceMask" + '-' + phase + '-' + m + '.png'))
+            plt.clf()
+
+    model.load_state_dict(best_model_wts)
+
+    return model
+
+
+def evaluation(dataloaders, model, class_names):
+    with torch.no_grad():
+        correct = 0
+        total = 0
+
+        for images, labels in dataloaders['test']:
+            images = images.to(DEVICE)
+            labels = labels.to(DEVICE)
+
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+        print('Accuracy of the model on the test images: {}%' \
+              .format(100 * correct / total))
+
+    with torch.no_grad():
+        inputs, labels = iter(dataloaders['test']).next()
+        inputs = inputs.to(DEVICE)
+
+        # inp = torchvision.utils.make_grid(inputs)
+
+        outputs = model(inputs)
+        _, preds = torch.max(outputs, 1)
+
+        for j in range(len(inputs)):
+            print("Acutal label", class_names[np.array(labels)[j]])
+            inp = inputs.data[j]
+            imshow(inp, 'predicted:' + class_names[preds[j]])
+
+
+def print_labeled_samples(dataloaders, class_names):
+    inputs, classes = next(iter(dataloaders['test']))
+
+    out = torchvision.utils.make_grid(inputs)
+
+    imshow(out, title=[class_names[x] for x in classes])
+
+
+class CNN(nn.Module):
+    def __init__(self, class_num):
+        super().__init__()
+        self.nclasses = class_num
+        self.in_channels = 3
+        self.loss_func = nn.CrossEntropyLoss()
+        self.feature_extractor = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2), )
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(256 * 28 * 28, 1024),
+            nn.BatchNorm1d(1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Linear(512, self.nclasses))
+
+    def forward(self, x):
+        features = self.feature_extractor(x)
+        class_scores = self.classifier(features)
+        return class_scores
+
---
+++ src/NaturalImages.py
@@ -0,0 +1,104 @@
+import os
+import argparse
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+from torch.optim import lr_scheduler
+from torchvision import models
+from FaceMaskClassificationUtils import imshow, DEVICE, CPU_DEVICE, split_prepare_dataset, CNN, train_model, evaluation, test_transform
+from PIL import Image
+
+
+natural_image_class_names_list = ['airplane', 'car', 'cat', 'dog', 'flower', 'fruit','motorbike', 'person']
+NUM_OF_NATURAL_IMAGE_CLASSES = len(natural_image_class_names_list)
+
+batch_size = 8
+num_workers = 4
+
+_num_epochs = 10
+_train_size, _validation_size, _test_size = 0.7, 0.15, 0.15
+
+
+def print_labeled_samples(dataloaders, class_names):
+    inputs, classes = next(iter(dataloaders['test']))
+
+    out = torchvision.utils.make_grid(inputs)
+
+    imshow(out, title=[class_names[x] for x in classes])
+
+
+def classify_natural_image(image_path, model):
+    """
+    Determines if and how a human wears a mask
+    :param img: the human image to classify
+    :param model: the model to be used
+    :return: Mask enum (MASK_WORN_INCORRECT, WITH_MASK, WITHOUT_MASK)
+    """
+    img = test_transform(Image.open(image_path))
+    with torch.no_grad():
+        img = img.to(CPU_DEVICE)
+        model.to(CPU_DEVICE)
+        class_prediction = torch.argmax(model(img.unsqueeze(0))).item()
+        mask = natural_image_class_names_list[class_prediction]
+        print("classify_mask_usage: this is a {}.".format(mask))
+        return mask
+
+
+def train_natural_image_detection(model_path, data_dir, class_num):
+    """
+    train the model
+    :return: path to the model
+    """
+    dataloaders, total_batch_sizes, class_names = split_prepare_dataset(data_dir, num_workers, batch_size)
+
+    model = models.resnet18(pretrained=True)
+    num_ftrs = model.fc.in_features
+    model.fc = nn.Linear(num_ftrs, class_num)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer_ft = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+    model = train_model(model, criterion, optimizer_ft, exp_lr_scheduler, _num_epochs, dataloaders, total_batch_sizes, batch_size)
+    model.eval()
+    evaluation(dataloaders, model, class_names)
+
+    # save model
+    checkpoint = {'model': models.resnet18(),
+                  'state_dict': model.state_dict()}
+    if os.path.exists(model_path):
+        os.remove(model_path)
+    torch.save(checkpoint, model_path)
+
+
+def load_natural_image_model(model_path, class_num):
+    checkpoint = torch.load(model_path, map_location=DEVICE)
+    model = checkpoint['model']
+    num_ftrs = model.fc.in_features
+    model.fc = nn.Linear(num_ftrs, class_num)
+    model.load_state_dict(checkpoint['state_dict'])
+    model.eval()
+    return model
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Description of your program')
+    parser.add_argument('-M', '--model_path', help='Model Path', required=True)
+    parser.add_argument('-D', '--data_path', help='Data Path', required=False)
+    parser.add_argument('-F', '--image_path', help='image to classify', required=True)
+    parser.add_argument('--train', dest='train', action='store_true')
+    parser.set_defaults(train=False)
+    args = parser.parse_args()
+    print(args)
+
+    # train if necessary
+    if args.train:
+        train_natural_image_detection(args.model_path, args.data_path, NUM_OF_NATURAL_IMAGE_CLASSES)
+
+    # load the model
+    model = load_natural_image_model(args.model_path, NUM_OF_NATURAL_IMAGE_CLASSES)
+
+    # classify the requested image
+    classify_natural_image(args.image_path, model)
